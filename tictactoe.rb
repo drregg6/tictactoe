77adaaa2992d50ff544eb4d350e033eb145ef085
Dave <drregg6@gmail.com>
2018-07-29 11:03:42 -0400

    @table needs to be defined when a new game is created

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,5 @@
 class TicTacToe
-    attr_accessor :player, :players, :gameboard
+    attr_accessor :player, :players, :gameboard, :table, :puts_table
     attr_reader :show_board
 
     def initialize(p1, p2)
@@ -7,14 +7,30 @@ class TicTacToe
         @p2 = Player.new(p2)
         @players = []
         @players << @p1 << @p2
+        # new table needs to be built with each new game
+        @table = {
+            top_left: nil,
+            top_mid: nil,
+            top_right: nil,
+            mid_left: nil,
+            mid_mid: nil,
+            mid_right: nil,
+            bot_left: nil,
+            bot_mid: nil,
+            bot_right: nil
+        }
+    end
+
+    def puts_table
+        puts @table
     end
 
     def make_board
 
         # this will have if statements
         # if :top_left = nil '_' else whoever owns the square
-
-        @top_left = '_'
+        # @top_left = '_' if @table[:top_left] == nil
+        @top_left = @table[:top_left] == nil ? '_' : @table[:top_left]
         @top_mid = '|_|'
         @top_right = '_'
         @mid_left = '_'
@@ -32,17 +48,6 @@ class TicTacToe
     end
 
     def show_board
-        # @board = %q(
-        #      |     |
-        #   x  |  x  |  x
-        # _____|_____|_____
-        #      |     |
-        #   x  |  x  |  x
-        # _____|_____|_____
-        #      |     |
-        #   x  |  x  |  x
-        #      |     |
-        #  )
         self.make_board.each do |row|
             puts row.join
         end
@@ -60,4 +65,6 @@ end
 
 my_game = TicTacToe.new("Dave", "Regg")
 puts my_game.players[1].name
+puts my_game.players
 my_game.show_board
+# my_game.puts_table
