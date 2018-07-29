c93cfe28017154d43ea59859206065f684f7d66b
Dave <drregg6@gmail.com>
2018-07-29 11:29:28 -0400

    awkward gameplay at work

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,8 +3,8 @@ class TicTacToe
     attr_reader :show_board
 
     def initialize(p1, p2)
-        @p1 = Player.new(p1)
-        @p2 = Player.new(p2)
+        @p1 = Player.new(p1, symbol="X")
+        @p2 = Player.new(p2, symbol="O")
         @players = []
         @players << @p1 << @p2
         # new table needs to be built with each new game
@@ -21,19 +21,15 @@ class TicTacToe
         }
     end
 
-    def puts_table
-        puts @table
-    end
-
     def make_board
         @top_left = @table[:top_left] == nil ? '_' : @table[:top_left]
-        @top_mid = @table[:top_mid] == nil ? '|_|' : @table[:top_mid]
+        @top_mid = @table[:top_mid] == nil ? '|_|' : "|#{@table[:top_mid]}|"
         @top_right = @table[:top_right] == nil ? '_' : @table[:top_right]
         @mid_left = @table[:mid_left] == nil ? '_' : @table[:mid_left]
-        @mid_mid = @table[:mid_mid] == nil ? '|_|' : @table[:mid_mid]
+        @mid_mid = @table[:mid_mid] == nil ? '|_|' : "|#{@table[:mid_mid]}|"
         @mid_right = @table[:mid_right] == nil ? '_' : @table[:mid_right]
         @bot_left = @table[:bot_left] == nil ? ' ' : @table[:bot_left]
-        @bot_mid = @table[:bot_mid] == nil ? '| |' : @table[:bot_mid]
+        @bot_mid = @table[:bot_mid] == nil ? '| |' : "|#{@table[:bot_mid]}|"
         @bot_right = @table[:bot_right] == nil ? ' ' : @table[:bot_right]
 
         @board = [
@@ -49,18 +45,27 @@ class TicTacToe
         end
     end
 
+    def move(player, location)
+        if @table.has_key?(location) && @table[location] == nil
+            @table[location] = @players[0].symbol
+        end
+    end
+
     class Player
         attr_accessor :name
         attr_reader :symbol
 
-        def initialize(name)
+        def initialize(name, symbol)
             @name = name
+            @symbol = symbol
         end
     end
 end
 
 my_game = TicTacToe.new("Dave", "Regg")
 puts my_game.players[1].name
+puts my_game.players[1].symbol
 puts my_game.players
 my_game.show_board
-# my_game.puts_table
+my_game.move(@p1, :mid_mid)
+my_game.show_board
